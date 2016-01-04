4ec08baade8fde0943e5428d800efa28fa3ddc78
“630352501@qq.com <630352501@qq.com>
2016-01-04 18:01:51 +0800

    0104

---
+++ src/main/java/com/talker/sortManage/dao/SchoolDao.java
@@ -0,0 +1,17 @@
+package com.talker.sortManage.dao;
+/**  
+ * 创建时间 : 2015-12-30 下午4:04:27
+ * 项目名称 : talker  
+ * 创建人 : zdd
+ * 文件名称 ：SchoolDao.java  
+ * 说明 :  TODO
+ * 修改时间:
+ * 修改人 :
+ * 修改原因 : 
+ */
+
+public interface SchoolDao {
+	
+	public boolean addSchool();
+
+}
